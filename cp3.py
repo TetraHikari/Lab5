d42b543125d816fdd9f9de2651c3649bd827e0ec
MacBook Pro <admin@MacBooks-MacBook-Pro-2.local>
2023-12-28 14:06:29 +0700

    Hanoi

--- cp3.py
+++ cp3.py
@@ -43,7 +43,32 @@ class Disk(object):
             turtle.left(90)
         
         turtle.end_fill()
+        
+class Hanoi(object):
+    def __init__(self, n=3, start="A", workspace="B", destination="C"):
+        self.startp = Pole(start, 0, 0)
+        self.workspacep = Pole(workspace, 150, 0)
+        self.destinationp = Pole(destination, 300, 0)
+        self.startp.showpole()
+        self.workspacep.showpole()
+        self.destinationp.showpole()
+        for i in range(n):
+            self.startp.pushdisk(Disk("d"+str(i), 0, i*150, 20, (n-i)*30))
 
+    def move_disk(self, start, destination):
+        disk = start.popdisk()
+        destination.pushdisk(disk)
+
+    def move_tower(self,n,s,d,w):
+        if n == 1:
+            self.move_disk(s,d)
+        else:
+            self.move_tower(n-1,s,w,d)
+            self.move_disk(s,d)
+            self.move_tower(n-1,w,d,s)
+
+    def solve(self):
+        self.move_tower(3,self.startp,self.destinationp,self.workspacep)
 
 if __name__ == "__main__":
     turtle.speed(1)  
@@ -52,3 +77,7 @@ if __name__ == "__main__":
     disk1.newpos(50, 50)
     disk1.cleardisk()
     turtle.done()  
+
+h = Hanoi()
+h.solve()
+
