71b5aa10d0728def0ae41cf40f7ddf6d247da32a
MacBook Pro <admin@MacBooks-MacBook-Pro-2.local>
2023-12-28 13:35:19 +0700

    hello

--- main.py
+++ main.py
@@ -6,6 +6,41 @@ from PySide6.QtGui import *
 from PySide6.QtMultimedia import QSoundEffect
 import random
 
+class Dinosaur:
+    def __init__(self):
+        self.image = QPixmap("images/dinosaur.png") 
+        self.jump_sound = QSoundEffect()
+        self.jump_sound.setSource(QUrl.fromLocalFile("sounds/sound2.wav"))
+        self.x = 50
+        self.y = 300
+        self.w = 40
+        self.h = 40
+        self.is_jumping = False
+        self.jump_speed = 15
+
+    def draw(self, p):
+        p.drawPixmap(QRect(self.x, self.y, self.w, self.h), self.image)
+
+    def jump(self):
+        if not self.is_jumping:
+            print("Jump!")
+            self.jump_sound.play()
+            self.is_jumping = True
+
+    def update_position(self):
+        if self.is_jumping:
+            self.y -= self.jump_speed
+            self.jump_speed -= 1  # gravity
+
+            # Check if the dinosaur has reached the ground
+            if self.y >= 300:
+                self.y = 300
+                self.is_jumping = False
+                self.jump_speed = 15
+        else:
+            # Reset the jump speed if not jumping
+            self.jump_speed = 15
+
 class Obstacle:
     def __init__(self):
         self.image = QPixmap("images/spike.png") 
