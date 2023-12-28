f64b5037f32c26146819cde47a2dd0721ac6b746
MacBook Pro <admin@MacBooks-MacBook-Pro-2.local>
2023-12-28 13:56:12 +0700

    walk class

--- cp2.py
+++ cp2.py
@@ -19,9 +19,17 @@ class Train(Transportation):
         total_station = self.end_place - self.start_place
         return total_station * 5
 
+#Pee:Walk class
+class Walk(Transportation):
+    def calculate_fare(self):
+        return 0
+
+
 if __name__ == "__main__":
     t = Train(10, 20)
     print(t.calculate_fare())
+    w = Walk(10, 20)
+    print(w.calculate_fare())
 
     
     
